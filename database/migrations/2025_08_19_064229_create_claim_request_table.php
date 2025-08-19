684a7474a0f7b486c0ac85f5953b6fbe01033329
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-08-19 12:15:26 +0530

    ADD Migration claim request

---
+++ database/migrations/2025_08_19_064229_create_claim_request_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('claim_request', function (Blueprint $table) {
+            $table->id();
+            $table->text('message');
+            $table->boolean('is_approved');
+            $table->foreignUuid('user_id')->constrained('user')->onDelete('cascade');
+            $table->foreignUuid('post_id')->constrained('post')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('claim_request');
+    }
+};
