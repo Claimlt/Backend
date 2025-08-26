066a6dbd028fd25ee7ac94f1d8a6e0f55bfda546
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-08-26 23:56:51 +0530

    add image upload controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ImageController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,4 +13,5 @@ Route::post("/register", [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
 Route::post("/logout", [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::post("/images", [ImageController::class, 'store'])->middleware('auth:sanctum')->name("image-upload");
 Route::apiResource("/posts", PostController::class)->middleware('auth:sanctum');
