2c8c1c72bc0f5166baf42edd664de1a06e18c940
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-08-26 23:55:34 +0530

    add image model

---
+++ app/Models/Image.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Image extends Model
+{
+    use HasUuids;
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'image';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = ['filename', 'imageable_type'];
+
+    public function imageable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    //  UUID settings
+    public $incrementing = false;   // no auto increment
+    protected $keyType = 'string';  // primary key is a string
+
+
+}
