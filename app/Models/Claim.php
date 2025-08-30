eac9830879fa25de7a24c568d6014593ea8397ac
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-08-30 19:11:32 +0530

    updated for uuid in claims

--- app/Models/Claim.php
+++ app/Models/Claim.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -11,6 +12,7 @@ class Claim extends Model
 {
     /** @use HasFactory<\Database\Factories\ClaimFactory> */
     use HasFactory;
+    use HasUuids;
 
     /**
      * The table associated with the model.
@@ -65,4 +67,8 @@ class Claim extends Model
     {
         return $this->morphMany(Image::class, 'imageable');
     }
+
+    //  UUID settings
+    public $incrementing = false;   // no auto increment
+    protected $keyType = 'string';  // primary key is a string
 }
