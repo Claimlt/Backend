8118b3cc328baad4820b034692f7284786d771c8
Pasidu Ranasinghe <104724063+PasiduRanasinghe@users.noreply.github.com>
2025-08-30 20:26:19 +0530

    add validation on store claim request

---
+++ app/Http/Requests/StoreClaimRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreClaimRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'post' => 'required|uuid|exists:post,id',
+            'message' => 'required|string',
+            'images' => 'required|array|max:5',
+            'images.*' => 'required|uuid|exists:image,id',
+        ];
+    }
+}
