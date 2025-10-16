9d212c7322db462e215892d7356d46dc38ea81c3
Supun Nethsara <157266532+SupunNethsara@users.noreply.github.com>
2025-10-15 22:31:27 -0700

    Make new endpoint to get trending topics

--- routes/api.php
+++ routes/api.php
@@ -12,17 +12,22 @@ Route::post("/register", [AuthController::class, 'register']);
 Route::post("/login", [AuthController::class, 'login']);
 Route::post("/logout", [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::apiResource("/users", UserController::class)->middleware('auth:sanctum');
+
 Route::get("/profile", [ProfileController::class, 'profile'])->middleware('auth:sanctum');
 Route::put("/profile", [ProfileController::class, 'update'])->middleware('auth:sanctum');
 Route::post("/profile-avatar", [ProfileController::class, 'updateAvatar'])->middleware('auth:sanctum');
 
 Route::post("/images", [ImageController::class, 'store'])->middleware('auth:sanctum')->name("image-upload");
 Route::apiResource("/posts", PostController::class)->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->get('/trending-titles', [PostController::class, 'trendingTitles']);
+
 Route::middleware('auth:sanctum')->get('/my-posts', [PostController::class, 'myPosts']);
 Route::apiResource("/claims", ClaimController::class)->middleware('auth:sanctum');
 Route::get("posts/{post}/claims", [ClaimController::class, 'getByPost'])->middleware('auth:sanctum');
 Route::get("/my-claims", [ClaimController::class, 'getByUser'])->middleware('auth:sanctum');
 Route::get("/my-post-claims", [ClaimController::class, 'getByUserPosts'])->middleware('auth:sanctum');
 Route::post('claims/{claim}/approve', [ClaimController::class, 'approve'])->middleware('auth:sanctum');
-Route::apiResource("/users", UserController::class)->middleware('auth:sanctum');
+
+
 
